4ecf9ed454e99cc4df1a45ac0b6c19549c8d5a7a
Amala Mary Paul <amalamarypaul@gmail.com>
2020-02-26 16:52:19 +0530

    Initial commit - dummy router

---
+++ src/About.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About:React.FC=()=>{
+  return(
+    <h2>Brush up about</h2>
+  )
+}
+
+export default About;
---
+++ src/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home:React.FC=()=>{
+  return(
+    <h1>Brushup home</h1>
+  )
+}
+
+export default Home;
